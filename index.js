f07505d7e1e49139e659f20c6be26a55692b4643
Thuc Tran <thuc.tv@techchain.vn>
2021-12-22 15:31:16 +0700

    update api database

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyparser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const data = [
-  {
-    id: 1,
-    title: 'Post title 1',
-  },
-  {
-    id: 2,
-    title: 'Post title 2',
-  },
-  {
-    id: 3,
-    title: 'Post title 3',
-  },
-  {
-    id: 4,
-    title: 'Post title 4',
-  },
-  {
-    id: 5,
-    title: 'Post title 5',
-  },
-  {
-    id: 6,
-    title: 'Post title 6',
-  },
-];
-app.get('/posts', cors(), (req, res) => res.send(data));
+require('dotenv').config()
+app.use(cors());
+app.use(bodyparser.json());
+
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+// api
+app.get('/posts', (req, res) => {
+  con.query('SELECT * FROM posts ORDER BY id asc', (err, rows) => {
+    if (err) throw err;
+
+    res.send(rows);
+  });
+});
+
+app.post('/posts', (req, res) => {
+  let post = req.body;
+  console.log(post);
+  con.query('INSERT INTO posts SET ?', post, (err, row) => {
+    if (err) throw err;
+
+    const data = { id: row.insertId };
+    res.send(data);
+  });
+});
+
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
