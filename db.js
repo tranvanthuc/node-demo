3ec2d8a514c6aec08750ea7480864390556013d7
Thuc Tran <thuc.tv@techchain.vn>
2021-12-23 10:34:19 +0700

    pool cluster mysql

---
+++ db.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+exports.READ = 'READ';
+exports.WRITE = 'WRITE';
+
+const pool = mysql.createPoolCluster();
+
+pool.add(exports.READ, {
+  host: process.env.READ_DB_HOST,
+  user: process.env.READ_DB_USER,
+  password: process.env.READ_DB_PASSWORD,
+  database: process.env.READ_DB_DATABASE,
+});
+
+pool.add(exports.WRITE, {
+  host: process.env.WRITE_DB_HOST,
+  user: process.env.WRITE_DB_USER,
+  password: process.env.WRITE_DB_PASSWORD,
+  database: process.env.WRITE_DB_DATABASE,
+});
+
+exports.get = function (type, done) {
+  if (!pool) return done(new Error('Missing database connection.'));
+
+  done(null, pool.of(type));
+};
--- index.js
+++ index.js
@@ -3,44 +3,40 @@ const app = express();
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
 app.use(cors());
 app.use(bodyparser.json());
-
-const mysql = require('mysql');
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
+const db = require('./db.js');
 
 // api
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/posts', (req, res) => {
-  con.query('SELECT * FROM posts ORDER BY id asc', (err, rows) => {
-    if (err) throw err;
 
-    res.send(rows);
+app.get('/posts', (req, res) => {
+  db.get(db.READ, function (err, connection) {
+    if (err) res.send('Database problem');
+
+    connection.query('SELECT * FROM posts ORDER BY id asc', (err, rows) => {
+      if (err) throw err;
+
+      res.send(rows);
+    });
   });
 });
 
 app.post('/posts', (req, res) => {
-  let post = req.body;
-  console.log(post);
-  con.query('INSERT INTO posts SET ?', post, (err, row) => {
-    if (err) throw err;
+  db.get(db.WRITE, function (err, connection) {
+    if (err) res.send('Database problem');
 
-    const data = { id: row.insertId };
-    res.send(data);
+    let post = req.body;
+    console.log(post);
+    con.query('INSERT INTO posts SET ?', post, (err, row) => {
+      if (err) throw err;
+
+      const data = { id: row.insertId };
+      res.send(data);
+    });
   });
 });
-
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
