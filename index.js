1659d4bd67547b4fe13a6a0338e23b0357561aab
Thuc Tran <thuc.tv@techchain.vn>
2022-07-01 16:47:32 +0700

    check connect mysql

--- index.js
+++ index.js
@@ -9,17 +9,43 @@ app.listen(PORT, () => {
 });
 app.use(cors());
 app.use(bodyparser.json());
-const db = require('./db.js');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.READ_DB_HOST,
+  user: process.env.READ_DB_USER,
+  password: process.env.READ_DB_PASSWORD,
+  database: process.env.READ_DB_DATABASE
+});
 
 // api
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/posts', (req, res) => {
-  db.get(db.READ, function (err, connection) {
-    if (err) res.send('Database problem');
+app.get('/check', (req, res) => {
+  connection.connect(function(err) {
+    if (err) {
+      throw err;
+    }
+  
+    res.send('Connected');
+  });
+});
 
+app.get('/posts', (req, res) => {
+  const connection = mysql.createConnection({
+    host: process.env.READ_DB_HOST,
+    user: process.env.READ_DB_USER,
+    password: process.env.READ_DB_PASSWORD,
+    database: process.env.READ_DB_DATABASE
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      throw err;
+    }
+  
     connection.query('SELECT * FROM posts ORDER BY id asc', (err, rows) => {
       if (err)  console.log(err);
 
